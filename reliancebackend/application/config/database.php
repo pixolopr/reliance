cb70aba7281c0f8f2734622af5b5bbcfef357bc6
Abhay Amin <pixoloproductions@gmail.com>
2015-03-10 01:43:24 +0530

    frontend

--- reliancebackend/application/config/database.php
+++ reliancebackend/application/config/database.php
@@ -97,7 +97,7 @@ else
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = 'reliance';
+$db['default']['database'] = 'wohligco_reliance';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
